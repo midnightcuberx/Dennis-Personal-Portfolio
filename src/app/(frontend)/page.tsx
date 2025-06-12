f25ac7af2d9a12af5fe4ecf601e54df4360c10d5
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-06-12 21:26:24 +1200

    prettier + add form component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,11 +4,8 @@ import Timeline from '@/components/Timeline/Timeline'
 import TypingText from '@/components/TypingText/TypingText'
 import ContentService from '@/data-layer/ContentService'
 import DataService from '@/data-layer/DataService'
-import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri'
-import { AiOutlinePython } from 'react-icons/ai'
-import { FaJava } from 'react-icons/fa'
-import { SiTypescript } from 'react-icons/si'
-import { FaHtml5 } from 'react-icons/fa'
+import Skills from '@/components/Skills/Skills'
+import ContactForm from '@/components/ContactForm/ContactForm'
 
 export default async function HomePage() {
   const homeData = await ContentService.getHomeData()
@@ -55,46 +52,13 @@ export default async function HomePage() {
         <Timeline timelineElements={workExperience} />
       </div>
       <div className="bg-navy-deep">
-        {/*Wave Transition*/}
-        <div className="relative w-full h-24 bg-white overflow-hidden">
-          <svg
-            className="absolute inset-0 w-full h-full"
-            viewBox="0 0 50 25"
-            preserveAspectRatio="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="50" height="25" fill="white" />
-            <polygon points="50,25 0,0 0,25" className="fill-navy-deep" />
-          </svg>
-        </div>
-        {/*Skills Section*/}
-        <div className="flex justify-center group p-4">
-          <h1 className="text-3xl font-bold relative inline-block">
-            Skills
-            <span className="absolute left-0 -bottom-1 h-1 bg-white w-full origin-left scale-x-0 group-hover:scale-x-100 transition-transform duration-500" />
-          </h1>
-        </div>
-        <div className="bg-navy-deep grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-4 p-10">
-          <RiTailwindCssFill size={200} className="mx-auto my-10" />
-          <RiNextjsFill size={200} className="mx-auto my-10" />
-          <AiOutlinePython size={200} className="mx-auto my-10" />
-          <FaJava size={200} className="mx-auto my-10" />
-          <SiTypescript size={200} className="mx-auto my-10" />
-          <FaHtml5 size={200} className="mx-auto my-10" />
-        </div>
-
-        <div className="relative w-full h-64 bg-white overflow-hidden">
-          <svg
-            className="absolute inset-0 w-full h-full"
-            viewBox="0 0 50 50"
-            preserveAspectRatio="none"
-          >
-            <polygon points="50,0 0,0 0,25" className="fill-navy-deep" />
-          </svg>
-        </div>
+        <Skills />
       </div>
-      {/*Projects Section*/}
       {/*Contact Section*/}
+      <div>
+        <ContactForm />
+      </div>
+
       <Footer />
     </div>
   )
