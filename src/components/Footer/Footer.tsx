6f250b3fd35651752fcc4960f6405f0cee670e06
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-06-12 16:33:20 +1200

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer: React.FC = () => {
   return (
     <div className="bg-navy-deep grid grid-cols-2">
-      <div></div>
+      <div className="text-white text-center py-4">
+        <p>Made by Dennis Hu</p>
+        <p>Powered by Payload CMS and Tailwind CSS</p>
+      </div>
+      <div className="text-white text-center py-4">
+        <Link href="https://www.linkedin.com/in/dennishu811/">
+          <p>LinkedIn</p>
+        </Link>
+        <Link href="https://github.com/midnightcuberx">
+          <p>GitHub</p>
+        </Link>
+      </div>
     </div>
   )
 }
