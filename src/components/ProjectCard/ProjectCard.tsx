6600b8d8cc5f979d62249585cbcfaf3c6c69668c
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-06-13 19:30:27 +1200

    Update ProjectCard.tsx

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,19 +1,33 @@
 'use client'
 import { Project } from '@/payload-types'
 import React, { memo } from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+} from '../ui/card'
+import { Badge } from '../ui/badge'
 
 const ProjectCard: React.FC<Project> = memo(({ projectName, description, date, tags }) => {
   return (
-    <div className="bg-navy-deep">
-      <h3>{projectName}</h3>
-      <p>{description}</p>
-      <time>{date}</time>
-      <ul>
+    <Card className="bg-white outline outline-1 outline-navy-glow hover:outline-navy-deep transition-all duration-300 max-w-[500px]">
+      <CardHeader>
+        <CardTitle className="text-navy-glow">{projectName}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="text-navyglow">
+        <time>{date}</time>
         {tags.map((tag) => (
-          <li key={tag.tag}>{tag.tag}</li>
+          <Badge key={tag.tag} variant="outline" className="ml-2">
+            {tag.tag}
+          </Badge>
         ))}
-      </ul>
-    </div>
+      </CardContent>
+    </Card>
   )
 })
 
