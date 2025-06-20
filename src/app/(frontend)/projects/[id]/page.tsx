c20103d00eff8e60ce5e64711b6815e080b1a379
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-06-20 23:54:33 +1200

    Add Project Page + add link in projectCard

---
+++ src/app/(frontend)/projects/[id]/page.tsx
@@ -0,0 +1,36 @@
+//'use cache'
+
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
+import Handlers from '@/handlers/Handlers'
+import ProjectCard from '@/components/ProjectCard/ProjectCard'
+import { redirect } from 'next/navigation'
+
+interface ProjectPageProps {
+  params: Promise<{ id: string }>
+}
+
+export default async function ProjectPage({ params }: ProjectPageProps) {
+  const project = await Handlers.getProject((await params).id)
+  if (!project) {
+    redirect('/404')
+  }
+
+  return (
+    <div className="flex flex-col">
+      <div className="min-h-screen mb-10">
+        <Navbar
+          navElements={[
+            { href: '/projects', text: 'Projects' },
+            { href: '/leetcode', text: 'Leetcode' },
+          ]}
+        />
+        <div className="mt-30 mx-[5%] grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 p-4">
+          <ProjectCard key={project.id} {...project} />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
