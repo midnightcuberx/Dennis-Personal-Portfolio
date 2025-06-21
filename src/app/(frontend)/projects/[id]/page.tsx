fb9c81a0a92a9e1c492c5303a3ce9be5461f7698
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-06-21 14:27:23 +1200

    Update page.tsx

--- src/app/(frontend)/projects/[id]/page.tsx
+++ src/app/(frontend)/projects/[id]/page.tsx
@@ -26,13 +26,12 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
         <div className="mt-30 mx-[5%] w-full md:w-[75%] lg:w-[50%]">
           <div className="flex flex-col gap-6 items-center justify-center">
             <h1 className="text-3xl font-bold mb-4 text-navy-deep">{project.projectName}</h1>
-            <Image
+            <img
               src={`/${project.image}`}
               alt={project.projectName}
               width={600}
               height={450}
               className="w-full h-auto rounded-lg mb-6"
-              quality={100}
             />
             <div className="flex flex-wrap gap-4 mt-4">
               {project.tags.map((tag) => (
